15b5f12a085f302205c4c96288fd66d584b2b9d1
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2024-11-02 00:08:18 -0300

    Add maven e plugins no VS

---
+++ projetoapdescomplica/src/main/java/com/projetoAP/projetoapdescomplica/ProjetoapdescomplicaApplication.java
@@ -0,0 +1,13 @@
+package com.projetoAP.projetoapdescomplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoapdescomplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoapdescomplicaApplication.class, args);
+	}
+
+}
---
+++ projetoapdescomplica/src/test/java/com/projetoAP/projetoapdescomplica/ProjetoapdescomplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoAP.projetoapdescomplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoapdescomplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
